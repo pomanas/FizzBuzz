ac119a95a71f845131417ad2ab7ac47a125fa12b
pomanas <slawomir.pomana@gmail.com>
2021-06-29 09:45:36 +0200

    First commit initialization FizzBuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlay
+{
+    class FizzBuzz
+    {
+        private string _result;
+
+        public FizzBuzz(string defaultResult)
+        {
+            _result = defaultResult;
+        }
+        public string GetStatus(int value)
+        {
+            var modulo3 = value % 3;
+            var modulo5 = value % 5;
+            if (modulo3 == 0 && modulo5 == 0 )
+            {
+                _result = "FizzBuzz";
+            }
+            else if (modulo3 == 0)
+            {
+                _result = "Fizz";
+            }
+            else if (modulo5 == 0)
+            {
+                _result = "Buzz";
+
+            }
+            else
+            {
+                _result = value.ToString();
+            }
+
+            return _result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoją liczbę:");
+            var userValue = GetNumber();
+            FizzBuzz fizzBuzz = new FizzBuzz("");
+            var result = fizzBuzz.GetStatus(userValue);
+            Console.WriteLine($"Twój wynik to: {result}");
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("Podaleś nieprawidłowe dane. Spróbuj ponownie");
+                    continue;
+                }
+                return userNumber;
+            }
+        }
+    }
+}
